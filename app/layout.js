0c21b60986a2c08d415e8ae6225f0260115aec28
nehagit0730 <154869005+nehagit0730@users.noreply.github.com>
2025-10-24 17:21:04 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,9 +14,9 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import ContactModal from "@/components/modals/ContactModal";
 import NewsletterModal from "@/components/modals/NewsletterModal";
 import SearchModal from "@/components/modals/SearchModal";
-import { Toaster } from "react-hot-toast"; // ✅ Toast added
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/next"
+// import { Toaster } from "react-hot-toast"; // ✅ Toast added
+// import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   useEffect(() => {
@@ -55,29 +55,29 @@ export default function RootLayout({ children }) {
           <NewsletterModal />
           <SearchModal />
           <BacktoTop />
-          <SpeedInsights/>
-          <Analytics/>
+          // <SpeedInsights/>
+          // <Analytics/>
         </Context>
-    <Toaster
-        toastOptions={{
-          duration: 3000,
-          style: {
-            background: "rgba(0, 0, 0, 0.85)",
-            color: "#fff",
-            fontSize: "16px",
-            padding: "16px 20px",
-            borderRadius: "12px",
-            maxWidth: "100%",
-          },
-        }}
-        containerStyle={{
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          position: "fixed",
-          zIndex: 9999,
-        }}
-      />
+    // <Toaster
+    //     toastOptions={{
+    //       duration: 3000,
+    //       style: {
+    //         background: "rgba(0, 0, 0, 0.85)",
+    //         color: "#fff",
+    //         fontSize: "16px",
+    //         padding: "16px 20px",
+    //         borderRadius: "12px",
+    //         maxWidth: "100%",
+    //       },
+    //     }}
+    //     containerStyle={{
+    //       top: "50%",
+    //       left: "50%",
+    //       transform: "translate(-50%, -50%)",
+    //       position: "fixed",
+    //       zIndex: 9999,
+    //     }}
+    //   />
       </body>
     </html>
   );
